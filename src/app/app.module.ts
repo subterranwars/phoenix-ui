33593fa80124eb920b6b3ffa19781288eca9254f
Markus von Rüden <m.v.rueden@open-email.de>
2020-03-02 13:11:31 +0100

    Add dummy header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ResourcesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
