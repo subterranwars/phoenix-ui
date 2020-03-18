952c381787b4b7436c899795eef9f5bedb560d9f
Chris <fafner83@gmx.de>
2020-03-18 11:54:18 +0100

    Notifications: Added buttons to read and remove them

--- src/app/modules/notifications/notifications.component.ts
+++ src/app/modules/notifications/notifications.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {PlayerService} from '../../services/player.service';
-import {Player} from '../../model/Player';
+import { PlayerService } from '../../services/player.service';
+import { Player } from '../../model/Player';
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+	selector: 'app-messages',
+	templateUrl: './notifications.component.html',
+	styleUrls: ['./notifications.component.scss']
 })
 export class NotificationComponent implements OnInit {
-  player: Player;
+	player: Player;
 
-  constructor(private playerService: PlayerService) { }
+	constructor(private playerService: PlayerService) { }
 
-  ngOnInit(): void {
-    this.playerService.getPlayer().subscribe((player: Player) => {
-      this.player = player;
-    });
-  }
+	ngOnInit(): void {
+		this.playerService.getPlayer().subscribe((player: Player) => {
+			this.player = player;
+		});
+	}
+
+	deleteNotification(id: number): void {
+
+	}
+	
+	markAsRead(id: number) : void {
+		
+	}
 }
