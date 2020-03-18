5902a669b331107aa707b896e8109948a0edd048
Markus von Rüden <m.v.rueden@open-email.de>
2020-03-18 21:38:57 +0100

    Wrap up notifications

--- src/app/modules/notifications/notifications.component.ts
+++ src/app/modules/notifications/notifications.component.ts
@@ -1,29 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { NotificationService } from '../../services/notification.service';
-import { PlayerService } from '../../services/player.service';
-import { Player } from '../../model/Player';
+import {Component, OnInit} from '@angular/core';
+import {NotificationService} from '../../services/notification.service';
+import {PlayerService} from '../../services/player.service';
+import {Player} from '../../model/Player';
 
 @Component({
-	selector: 'app-messages',
-	templateUrl: './notifications.component.html',
-	styleUrls: ['./notifications.component.scss']
+  selector: 'app-messages',
+  templateUrl: './notifications.component.html',
+  styleUrls: ['./notifications.component.scss']
 })
 export class NotificationComponent implements OnInit {
-	player: Player;
+  player: Player;
 
-	constructor(private playerService: PlayerService, private notificationService: NotificationService) { }
+  constructor(private playerService: PlayerService, private notificationService: NotificationService) {
+  }
 
-	ngOnInit(): void {
-		this.playerService.getPlayer().subscribe((player: Player) => {
-			this.player = player;
-		});
-	}
+  ngOnInit(): void {
+    this.playerService.getPlayer().subscribe((player: Player) => {
+      this.player = player;
+    });
+  }
 
-	deleteNotification(id: bigint): void {
-		this.notificationService.delete(id);
-	}
-	
-	markAsRead(id: bigint): void {
-		this.notificationService.markAsRead(id);
-	}
+  deleteNotification(id: bigint): void {
+    this.notificationService.delete(id);
+  }
+
+  markAsRead(id: bigint): void {
+    this.notificationService.markAsRead(id);
+  }
 }
